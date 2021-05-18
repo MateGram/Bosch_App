0f8426b0038c95b3599758bcbcd7e2c6d6e37c9c
Samed Vossberg <samedvossberg@gmail.com>
2021-05-18 15:48:34 +0200

    Added new files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 
+# url routing paths
 urlpatterns = [
+     path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,17 @@ from django.urls import path
 
 from . import views
 
-# Setting index call
+# setting app name 
+app_name = 'polls'
+
+# Setting url pattern within polls app
 urlpatterns = [
-    path('', views.index, name='index'),
+    # index call on polls app || ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # id call in polls  || ex: /polls/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # results on id || ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # Votes on choices || ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
